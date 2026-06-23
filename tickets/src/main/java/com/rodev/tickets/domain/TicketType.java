8916a67757d012cd7c419bf9a5833894b2a5b2f1
Rodrigo Sambayon <rodrigo.jr.sambayon@accenture.com>
2026-06-24 03:10:24 +0800

    Updated Ticket entity and created TicketValidation entity

---
+++ tickets/src/main/java/com/rodev/tickets/domain/TicketType.java
@@ -0,0 +1,49 @@
+package com.rodev.tickets.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tickets_types")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TicketType {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "total_available")
+    private Integer totalAvailable;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "events_id")
+    private Event event;
+
+    @OneToMany(mappedBy = "ticketType", cascade = CascadeType.ALL)
+    private List<Ticket> tickets;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", updatable = false, nullable = false)
+    private LocalDateTime updatedAt;
+
+}
