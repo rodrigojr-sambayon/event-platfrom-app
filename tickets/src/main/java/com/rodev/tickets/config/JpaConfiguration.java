439191680b1351e80edc8792d7b19595b9c13d65
Rodrigo Sambayon <rodrigo.jr.sambayon@accenture.com>
2026-06-24 12:15:48 +0800

    Created JpaConfigration class with @EnabaleJpaAuditing annotation & orm.xml config file enabling the audit fields

---
+++ tickets/src/main/java/com/rodev/tickets/config/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package com.rodev.tickets.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+}
