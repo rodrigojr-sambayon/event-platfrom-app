e809e2444c6bd6a453d800fbf7dbdc6c8eb556c8
Rodrigo Sambayon <rodrigo.jr.sambayon@accenture.com>
2026-06-27 17:19:34 +0800

    Created the EventController class and implemented create event endpoint

---
+++ tickets/src/main/java/com/rodev/tickets/controllers/EventController.java
@@ -0,0 +1,42 @@
+package com.rodev.tickets.controllers;
+
+import com.rodev.tickets.domain.CreateEventRequest;
+import com.rodev.tickets.domain.dtos.CreateEventRequestDto;
+import com.rodev.tickets.domain.dtos.CreateEventResponseDto;
+import com.rodev.tickets.domain.entities.Event;
+import com.rodev.tickets.mappers.EventMapper;
+import com.rodev.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+    private final EventMapper eventMapper;
+
+    @PostMapping
+    public ResponseEntity<CreateEventRequestDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDto createEventRequestDto) {
+
+        CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+
+        Event createdEvent = eventService.createEvent(userId, createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+        return new ResponseEntity<>(createEventRequestDto, HttpStatus.CREATED);
+    }
+}
