876bf39029895ddb8e1b14aecd990a390c5e83c0
Rodrigo Sambayon <rodrigo.jr.sambayon@accenture.com>
2026-06-27 15:29:45 +0800

    Update TicketType Class adding the field description

--- tickets/src/main/java/com/rodev/tickets/domain/TicketType.java
+++ tickets/src/main/java/com/rodev/tickets/domain/TicketType.java
@@ -30,6 +30,9 @@ public class TicketType {
     @Column(name = "price", nullable = false)
     private Double price;
 
+    @Column(name = "description")
+    private String description;
+
     @Column(name = "total_available")
     private Integer totalAvailable;
 
