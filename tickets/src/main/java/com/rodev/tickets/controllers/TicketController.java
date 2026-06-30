dfa2564d23a3d01cd40bf31f30a76387b7203a8f
Rodrigo Sambayon <rodrigo.jr.sambayon@accenture.com>
2026-06-30 12:50:17 +0800

    Implemented the get ticket endpoint

--- tickets/src/main/java/com/rodev/tickets/controllers/TicketController.java
+++ tickets/src/main/java/com/rodev/tickets/controllers/TicketController.java
@@ -1,17 +1,22 @@
 package com.rodev.tickets.controllers;
 
 
+import com.rodev.tickets.domain.dtos.GetTicketResponseDto;
 import com.rodev.tickets.domain.dtos.ListTicketResponseDto;
 import com.rodev.tickets.mappers.TicketMapper;
 import com.rodev.tickets.services.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 import static com.rodev.tickets.util.JwtUtil.parseUserId;
 
@@ -31,5 +36,15 @@ public class TicketController {
                 parseUserId(jwt),
                 pageable
         ).map(ticketMapper::toListTicketResponseDto);
+    }
+
+    @GetMapping(path = "/{ticketId}")
+    public ResponseEntity<GetTicketResponseDto> getTicket(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID ticketId){
+        return ticketService.getTicketForUser(parseUserId(jwt), ticketId)
+                .map(ticketMapper::toGetTicketResponseDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
